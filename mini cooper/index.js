641e7d8791ccb1bad509cb0ec464649a8cfcf430
Daniel Kuzminski <dankuzmina@gmail.com>
2024-04-16 14:45:49 +0200

    create animated hamburger menu

---
+++ mini cooper/index.js
@@ -0,0 +1,7 @@
+// const hamMenu = document.querySelector('.hamburger-menu')
+// const hamArea = document.querySelector('.ham')
+
+// hamMenu.addEventListener('click', () => {
+//     hamArea.classList.replace('hamburger-area','hamburger-area-full')
+// })
+
